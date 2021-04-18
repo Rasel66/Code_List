b2c2fcf218644671210d0d4f6bcf5f4319a3a694
Md. Rasel Hossain <59763029+Rasel66@users.noreply.github.com>
2021-04-18 17:56:01 +0600

    Add files via upload

---
+++ Array initialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,num[5]={12,13,14,15,16};
+    for(i=0;i<5;i++)
+    {
+        cout<<(i+1)<<" :: "<<num[i]<<endl;
+    }
+    return 0;
+}
